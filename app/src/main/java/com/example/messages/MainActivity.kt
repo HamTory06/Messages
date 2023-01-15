5791af1de69715b8d5d001c18ca024414ba99d44
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-01-15 23:41:36 +0900

    기본 메시지 메인 xml 작성

--- app/src/main/java/com/example/messages/MainActivity.kt
+++ app/src/main/java/com/example/messages/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.messages
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.messages.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var mbinging: ActivityMainBinding ?= null
+    private val binding get() = mbinging!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mbinging = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
