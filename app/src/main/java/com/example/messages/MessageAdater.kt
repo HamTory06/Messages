56e4e4b3d965ccf3c5587e15a589151d61c7a75c
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-01-17 18:25:13 +0900

    메시지 리사이클러뷰 추가

--- app/src/main/java/com/example/messages/MessageAdater.kt
+++ app/src/main/java/com/example/messages/MessageAdater.kt
@@ -2,10 +2,15 @@ package com.example.messages
 
 import android.content.Context
 import android.os.Message
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class MessageAdater(context: Context) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private val VIEW_TYPE_USER_MESSAGE_ME = 10
+    private val VIEW_TYPE_USER_MESSAGE_OTHER = 11
+
 
     private var messages: MutableList<BaseMessage>
     init {
@@ -22,14 +27,19 @@ class MessageAdater(context: Context) : RecyclerView.Adapter<RecyclerView.ViewHo
         notifyDataSetChanged()
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+        val layoutInflater = LayoutInflater.from(parent.context)
+
+        return when(viewType){
+            VIEW_TYPE_USER_MESSAGE_ME -> {
+                holder as MyUserHolder
+                holder.bindView(context, messages.get(position) as )
+            }
+        }
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = messages.size
 }
---
+++ app/src/main/java/com/example/messages/MyUserHolder.kt
@@ -0,0 +1,8 @@
+package com.example.messages
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class MyUserHolder(view: View) : RecyclerView.ViewHolder(view) {
+    val messageText = view.
+}
