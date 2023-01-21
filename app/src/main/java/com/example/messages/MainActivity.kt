d064958c8df3524e8ad47f9ac50d682f0af2015d
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-01-21 15:32:03 +0900

    메시지 공부

--- app/src/main/java/com/example/messages/MainActivity.kt
+++ app/src/main/java/com/example/messages/MainActivity.kt
@@ -1,15 +1,154 @@
 package com.example.messages
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.RecyclerView
 import com.example.messages.databinding.ActivityMainBinding
+import com.example.messages.MessageAdater
+import com.sendbird.android.*
+import com.sendbird.android.SendBird.ChannelHandler
+import kotlin.math.E
 
 class MainActivity : AppCompatActivity() {
-    private var mbinging: ActivityMainBinding ?= null
-    private val binding get() = mbinging!!
+
+    private var mbinding: ActivityMainBinding ?= null
+    private val binding get() = mbinding!!
+
+
+    private val EXTRA_CHANNEL_URL = "EXTRA_CHANNEL_URL"
+    private val CHANNEL_HANDLER_ID = "CHANNEL_HANDLER_GROUP_CHANNEL_CHAT"
+
+
+    private lateinit var adapter: MessageAdater
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var groupChannel: GroupChannel
+    private lateinit var channelUrl: String
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mbinging = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+
+
+        setUpRecyclerView()
+        setButtonListeners()
     }
+
+    override fun onResume() {
+        super.onResume()
+        channelUrl = getChannelURl()
+
+        GroupChannel.getChannel(channelUrl,
+            GroupChannel.GroupChannelGetHandler { groupChannel, e ->
+                if (e != null) {
+                    // Error!
+                    e.printStackTrace()
+                    return@GroupChannelGetHandler
+                }
+                this.groupChannel = groupChannel
+                getMessages()
+            })
+
+        SendBird.addChannelHandler(
+            CHANNEL_HANDLER_ID,
+            object : ChannelHandler() {
+                override fun onMessageReceived(
+                    baseChannel: BaseChannel,
+                    baseMessage: BaseMessage
+                ){
+                    if (baseChannel.url == channelUrl) {
+                        // Add new message to view
+                        adapter.addFirst(baseMessage)
+                        groupChannel.markAsRead()
+                    }
+                }
+            })
+    }
+
+    override fun onPause() {
+        super.onPause()
+        SendBird.removeChannelHandler(CHANNEL_HANDLER_ID)
+    }
+
+    /**
+     * Function handles setting handlers for back/send button
+     */
+    private fun setButtonListeners() {
+        val back = binding.buttonGchatBack
+        back.setOnClickListener {
+            val intent = Intent(this, ChannelListActivity::class.java)
+            startActivity(intent)
+        }
+
+        val send = binding.buttonSend
+        send.setOnClickListener {
+            sendMessage()
+        }
+    }
+
+    /**
+     * Sends the message from the edit text, and clears text field.
+     */
+    private fun sendMessage()
+    {
+        val params = UserMessageParams()
+            .setMessage(binding.editGcharMessage.text.toString())
+        groupChannel.sendUserMessage(params,
+            BaseChannel.SendUserMessageHandler { userMessage, e ->
+                if (e != null) {    // Error.
+                    return@SendUserMessageHandler
+                }
+                adapter.addFirst(userMessage)
+                binding.editGcharMessage.text.clear()
+            })
+    }
+
+
+    /**
+     * Function to get previous messages in channel
+     */
+    private fun getMessages() {
+
+        val previousMessageListQuery = groupChannel.createPreviousMessageListQuery()
+
+        previousMessageListQuery.load(
+            100,
+            true,
+            object : PreviousMessageListQuery.MessageListQueryResult {
+                override fun onResult(
+                    messages: MutableList<BaseMessage>?,
+                    e: SendBirdException?
+                ) {
+                    if (e != null) {
+                        e.message?.let { Log.e("Error", it) }
+                    }
+                    adapter.loadMessages(messages!!)
+                }
+            })
+
+    }
+
+    /**
+     * Set up the  recyclerview and set the adapter
+     */
+    private fun setUpRecyclerView() {
+        adapter = MessageAdater(this)
+        recyclerView = binding.recyclerGchat
+        recyclerView.adapter = adapter
+        val layoutManager = androidx.recyclerview.widget.LinearLayoutManager(this)
+        layoutManager.reverseLayout = true
+        recyclerView.layoutManager = layoutManager
+        recyclerView.scrollToPosition(0)
+
+    }
+
+    /**
+     * Get the Channel URL from the passed intent
+     */
+    private fun getChannelURl(): String {
+        val intent = this.intent
+        return intent.getStringExtra(EXTRA_CHANNEL_URL)
+    }
+
 }
