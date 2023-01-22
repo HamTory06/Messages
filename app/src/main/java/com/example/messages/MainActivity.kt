a49c8112f06d48a639e5a5142ce48cd38ea140b6
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-01-23 02:04:53 +0900

    ButtomNavigation

--- app/src/main/java/com/example/messages/MainActivity.kt
+++ app/src/main/java/com/example/messages/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
+import androidx.core.app.TaskStackBuilder
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
